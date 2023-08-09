ee41e1598bc5eba75c7c09332a1a4c0e8f5006e3
klarazogolli <klara.zogolli@gmail.com>
2023-08-09 15:09:15 +0200

    modified:   app.js 	new file:   controllers/products.js 	modified:   models/product.js 	modified:   package-lock.json 	modified:   package.json 	modified:   populate.js 	modified:   routes/products.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
-
+require('dotenv').config();
+require('express-async-errors');
 // async errors
-
 const express = require('express');
 const app = express();
-const connectDB = require()
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
 
@@ -15,6 +15,7 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('<h1>Store API</h1> <a href="/api/v1/products">Products route </a>')
 })
+app.use('/api/v1/products',productsRouter)
 // products route
 
 app.use(notFoundMiddleware);
@@ -25,6 +26,7 @@ const port = process.env.PORT || 3000
 const start = async ()=>{
     try{
         // connectDB
+        await connectDB(process.env.MONGO_URI)
         app.listen(port , console.log(`Server is listening in port: ${port}`))
 
     }catch(err){
